9cd369305b3ff0d205f3a17b1a87ca971b7a2c08
Muhammad Fajar Baihaqi <muhammadfajarbaihaqi27@gmail.com>
2024-02-05 19:48:18 +0700

    Menambahkan Program Handtracker Versi 1.0

---
+++ Handtracker.py
@@ -0,0 +1,26 @@
+# Required Library
+# 1. Mediapipe
+# 2. Opencv
+
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_style = mp.solutions.drawing_styles
+mphands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+hands = mphands.Hands()
+while True:
+    data,image = cap.read()
+    # Flip The Image
+    image = cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+    # Storing the Results
+    results = hands.process(image)
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks, mphands.HAND_CONNECTIONS)
+    cv2.imshow('Handtracker', image)            
+    cv2.waitKey(1)
